1ea9ae349a2b576a8fe63abcd537f9115b69a93b
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-10-19 15:12:13 -0400

    refactor getAllReservations to get reservation data using queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -22,6 +22,9 @@ const getUserWithEmail = function (email) {
   return pool
   .query(`SELECT * FROM users WHERE email = $1;`, [email])
   .then((result) => {
+    if (result.rows.length === 0) {
+      return null;
+    }
     return result.rows[0];
   })
   .catch((error) => {
@@ -69,7 +72,21 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    return result.rows
+  })
+  .catch((error) => {
+    console.log("An error occured", error)
+  })
 };
 
 /// Properties
